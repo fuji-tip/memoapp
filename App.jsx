7a7fa22709a15754e998bd5502c154354641f00e
fuji-tip <3615407+fuji-tip@users.noreply.github.com>
2021-11-29 17:24:39 +0900

    Updated lecture of props

--- App.jsx
+++ App.jsx
@@ -2,10 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// eslint-disable-next-line import/extensions
+import Hello from './src/components/Hello.jsx';
+
 // eslint-disable-next-line react/function-component-definition
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>world</Hello>
+      <Hello style={{fontSize: 16, backgroundColor: '#000'}}>unko</Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
